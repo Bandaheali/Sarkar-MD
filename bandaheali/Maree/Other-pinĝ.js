be91f9199b0cddf0be3cb29f27e884e500c45c48
Sarkar~Tech <bandahealimaree@gmail.com>
2025-03-25 09:39:27 +0500

    Update Other-pinĝ.js

--- bandaheali/Maree/Other-pinĝ.js
+++ bandaheali/Maree/Other-pinĝ.js
@@ -7,15 +7,38 @@ const ping = async (m, sock) => {
     : '';
 
   if (["ping", "speed", "p"].includes(cmd)) {
-    const start = performance.now();
+ 
     await m.React('⚡'); // React with lightning icon
 
+    const start = performance.now();
     const end = performance.now();
     const responseTime = (end - start).toFixed(2);
+    const responseText = `*⚡ SARKAR-MD SPEED:* *${responseTime}ms*`;
 
-    await sock.sendMessage(m.from, { 
-      text: `*SARKAR-MD SPEED:* *${responseTime}ms*`
-    }, { quoted: m });
+    await sock.sendMessage(
+      m.from,
+      {
+        text: responseText,
+        contextInfo: {
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+            newsletterJid: '120363315182578784@newsletter',
+            newsletterName: "Sarkar-MD",
+            serverMessageId: -1,
+          },
+          forwardingScore: 999, // Score to indicate it has been forwarded
+          externalAdReply: {
+            title: "✨ Sarkar-MD ✨",
+            body: "Ping Speed Calculation",
+            thumbnailUrl: 'https://raw.githubusercontent.com/Sarkar-Bandaheali/BALOCH-MD_DATABASE/refs/heads/main/Pairing/1733805817658.webp', // Thumbnail image
+            sourceUrl: 'https://github.com/Sarkar-Bandaheali/Sarkar-MD/fork', // Source link
+            mediaType: 1,
+            renderLargerThumbnail: false,
+          },
+        },
+      },
+      { quoted: m }
+    );
 
     await m.React('✅'); // Success reaction
   }
