d24ed5facf53e8fc015ffa4a612ef38d004757da
Sarkar~Tech <bandahealimaree@gmail.com>
2025-04-12 04:54:43 +0500

    Update Owner-antiCall.js

--- bandaheali/Maree/Owner-antiCall.js
+++ bandaheali/Maree/Owner-antiCall.js
@@ -5,7 +5,7 @@ const bot = await Matrix.decode.jid(Matrix.user.id);
 const dev = '923253617422@s.whatsapp.net';
 const Creater = [bot, dev, config.OWNER_NUMBER + '@s.whatsapp.net'].includes(m.sender);
 const prefix = config.PREFIX;
-const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : ";
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : "";
 
 const text = m.body.slice(prefix.length + cmd.length).trim();
 
@@ -26,7 +26,7 @@ responceMsg = "usage:\n 'anticall on` : enable antiCall\n`anticall off`: DisAble
 try { 
       await Matrix.sendMessage(m.from, {text: responceMsg }, {quoted: m });
       } catch (error) {
-      await matrix.sendMessage(m.from, { text: 'error processing your request' }, { quoted: m });
+      await Matrix.sendMessage(m.from, { text: 'error processing your request' }, { quoted: m });
       }
       }
       };
