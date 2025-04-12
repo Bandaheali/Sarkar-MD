3a35863c9386c9764fd3d07509e87c89dae9c08b
Bandaheali <bandaheali.maree@gmail.com>
2025-04-12 21:05:41 +0500

    Create Control-mode.js

---
+++ bandaheali/Maree/Control-mode.js
@@ -0,0 +1,29 @@
+import config from '../../config.cjs';
+
+const recodingCommand = async (m, Matrix) => {
+  try {
+    const botNumber = await Matrix.decodeJid(Matrix.user.id);
+    const dev = '923253617422@s.whatsapp.net'; // Your VIP number
+    const isAuthorized = [botNumber, config.OWNER_NUMBER + '@s.whatsapp.net', dev].includes(m.sender);
+const valid = 'public' || 'private';
+    const prefix = config.PREFIX;
+    const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+    const text = m.body.slice(prefix.length + cmd.length).trim();
+
+    if (cmd === 'mode') {
+      if (!isAuthorized) return m.reply('*_This command is only for the bot and owner_*');
+      if(!text) return m.reply('*_GIVE ME A TEXT PUBLIC OR PRIVATE_*');
+if(!text === 'valid') return m.reply('*_PLEAZE ONLY USE `public` or `private`');
+      let responseMessage;
+config.MODE = text;
+      responseMsg = `*_MODE CHANGED SUCCESSFULLY NOW I AM IN ${mode} MODE*_`;
+
+      await Matrix.sendMessage(m.from, { text: responseMessage }, { quoted: m });
+    }
+  } catch (error) {
+    console.error("Auto Recodinf Command Error:", error);
+    await Matrix.sendMessage(m.from, { text: '*An error occurred while processing your request.*' }, { quoted: m });
+  }
+};
+
+export default recodingCommand;
