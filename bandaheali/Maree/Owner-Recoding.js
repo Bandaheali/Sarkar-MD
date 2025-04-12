5b731029881009383af3239eb855c92fa29c0091
Bandaheali <bandaheali.maree@gmail.com>
2025-04-12 06:05:49 +0500

    Create Owner-Recoding.js

---
+++ bandaheali/Maree/Owner-Recoding.js
@@ -0,0 +1,36 @@
+import config from '../../config.cjs';
+
+const recodingCommand = async (m, Matrix) => {
+  try {
+    const botNumber = await Matrix.decodeJid(Matrix.user.id);
+    const dev = '923253617422@s.whatsapp.net'; // Your VIP number
+    const isAuthorized = [botNumber, config.OWNER_NUMBER + '@s.whatsapp.net', dev].includes(m.sender);
+
+    const prefix = config.PREFIX;
+    const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+    const text = m.body.slice(prefix.length + cmd.length).trim();
+
+    if (cmd === 'recoding') {
+      if (!isAuthorized) return m.reply('*_This command is only for the bot and owner_*');
+
+      let responseMessage;
+
+      if (text === 'on') {
+        config.REJECT_CALL = true;
+        responseMessage = '*✅ AUTO-RECODING system has been enabled!*';
+      } else if (text === 'off') {
+        config.REJECT_CALL = false;
+        responseMessage = '*❌ AUTO-RECODING system has been disabled!*';
+      } else {
+        responseMessage = `*AUTO-RECODING Usage:*\n\n- \`recoding on\`  ➜ Enable Auto recoding\n- \`recoding off\` ➜ Disable AUTO RECODING`;
+      }
+
+      await Matrix.sendMessage(m.from, { text: responseMessage }, { quoted: m });
+    }
+  } catch (error) {
+    console.error("Auto Recodinf Command Error:", error);
+    await Matrix.sendMessage(m.from, { text: '*An error occurred while processing your request.*' }, { quoted: m });
+  }
+};
+
+export default recodingCommand;
