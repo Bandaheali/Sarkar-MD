d8cbd6499c3047558841f31e6be160d76bd4d36e
Sarkar~Tech <bandahealimaree@gmail.com>
2025-04-12 05:02:20 +0500

    Update Owner-antiCall.js

--- bandaheali/Maree/Owner-antiCall.js
+++ bandaheali/Maree/Owner-antiCall.js
@@ -1,34 +1,34 @@
 import config from '../../config.cjs';
 
-const antiCall = async (m, Matrix) => {
-  const bot = await Matrix.decode.jid(Matrix.user.id);
-  const dev = '923253617422@s.whatsapp.net';
-  const Creater = [bot, dev, config.OWNER_NUMBER + '@s.whatsapp.net'].includes(m.sender);
+// Main command function
+const anticallcommand = async (m, Matrix) => {
+  const botNumber = await Matrix.decodeJid(Matrix.user.id);
+  const isCreator = [botNumber, config.OWNER_NUMBER + '@s.whatsapp.net'].includes(m.sender);
   const prefix = config.PREFIX;
-  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : "";
-
-  const text = m.body.slice(prefix.length + cmd.length).trim();
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
 
   if (cmd === 'anticall') {
-    if (!Creater) return m.reply("*_THIS COMMAND IS ONLY FOR BOT AND OWNER_*");
+    if (!isCreator) return m.reply("*Only admin*");
+    let responseMessage;
 
-    let responceMsg;
     if (text === 'on') {
       config.REJECT_CALL = true;
-      responceMsg = "antiCall has been enabled";
+      responseMessage = "Anti-Call has been enabled.";
     } else if (text === 'off') {
       config.REJECT_CALL = false;
-      responceMsg = "*_AntiCall has been Disabled_*";
+      responseMessage = "Anti-Call has been disabled.";
     } else {
-      responceMsg = "usage:\n`anticall on`: enable antiCall\n`anticall off`: disable antiCall";
+      responseMessage = "Usage:\n- `anticall on`: Enable Anti-Call\n- `anticall off`: Disable Anti-Call";
     }
 
     try {
-      await Matrix.sendMessage(m.from, { text: responceMsg }, { quoted: m });
+      await Matrix.sendMessage(m.from, { text: responseMessage }, { quoted: m });
     } catch (error) {
-      await Matrix.sendMessage(m.from, { text: 'error processing your request' }, { quoted: m });
+      console.error("Error processing your request:", error);
+      await Matrix.sendMessage(m.from, { text: 'Error processing your request.' }, { quoted: m });
     }
   }
 };
 
-export default antiCall;
+export default anticallcommand;
