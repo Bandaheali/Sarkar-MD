3424f021bb3c9182386a0298ceb8f41ae6b25684
Sarkar~Tech <bandahealimaree@gmail.com>
2025-04-18 12:44:58 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -14,6 +14,7 @@ const config = {
   AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
   AUTO_BIO: process.env.AUTO_BIO !== undefined ? process.env.AUTO_BIO === 'true' : false,
   CHAT_BOT: process.env.CHAT_BOT !== undefined ? process.env.CHAT_BOT === 'true' : false,
+  VOICE_BOT: process.env.VOICE_BOT !== undefined ? process.env.VOICE_BOT === 'true' : false,
   HEART_REACT: process.env.HEART_REACT !== undefined ? process.env.HEART_REACT === 'false' : false,
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   ANTI_DELETE: process.env.ANTI_DELETE !== undefined ? process.env.ANTI_DELETE === 'true' : true,
