a4765283e35e710648cf3fb805248f64ed419ad7
Sarkar~Tech <bandahealimaree@gmail.com>
2025-04-19 10:57:50 +0500

    Update dl-app.js

--- bandaheali/Maree/dl-app.js
+++ bandaheali/Maree/dl-app.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import config from "../config.cjs";
+import config from "../../config.cjs";
 
 const apkDownloader = async (m, Matrix) => {
   const prefix = config.PREFIX;
