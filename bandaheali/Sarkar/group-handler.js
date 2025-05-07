7bccf3b22a4fa8ea3f6e770c0b7fa8627d0f4d72
Bandaheali <bandaheali.maree@gmail.com>
2025-05-07 06:21:27 +0500

    Update group-handler.js

--- bandaheali/Sarkar/group-handler.js
+++ bandaheali/Sarkar/group-handler.js
@@ -1,60 +1,60 @@
 import moment from 'moment-timezone';
 import config from '../../config.cjs';
+
 export default async function GroupParticipants(sock, { id, participants, action }) {
-   try {
-      const metadata = await sock.groupMetadata(id)
+  try {
+    const metadata = await sock.groupMetadata(id);
 
-      // participants
-      for (const jid of participants) {
-         // get profile picture user
-         let profile
-         try {
-            profile = await sock.profilePictureUrl(jid, "image")
-         } catch {
-            profile = "https://lh3.googleusercontent.com/proxy/esjjzRYoXlhgNYXqU8Gf_3lu6V-eONTnymkLzdwQ6F6z0MWAqIwIpqgq_lk4caRIZF_0Uqb5U8NWNrJcaeTuCjp7xZlpL48JDx-qzAXSTh00AVVqBoT7MJ0259pik9mnQ1LldFLfHZUGDGY=w1200-h630-p-k-no-nu"
-         }
-
-         // action
-         if (action == "add" && config.WELCOME ) {
-           const userName = jid.split("@")[0];
-                    const joinTime = moment.tz('Asia/Pakistan').format('HH:mm:ss');
-                    const joinDate = moment.tz('Asia/Pakistan').format('DD/MM/YYYY');
-                    const membersCount = metadata.participants.length;
-            sock.sendMessage(id, {
-               text: `> Hello @${userName}! Welcome to *${metadata.subject}*.\n> You are the ${membersCount}th member.\n> Joined at: ${joinTime} on ${joinDate}
-"`, contextInfo: {
-                  mentionedJid: [jid],
-                  externalAdReply: {
-                     title: `Welcome`,
-                     mediaType: 1,
-                     previewType: 0,
-                     renderLargerThumbnail: true,
-                     thumbnailUrl: metadata.subject,
-                     sourceUrl: 'texhbybandali.blogspot.com'
-                  }
-               }
-            })
-         } else if (action == "remove" && config.WELCOME ) {
-           const userName = jid.split('@')[0];
-                    const leaveTime = moment.tz('Asia/pakistan').format('HH:mm:ss');
-                    const leaveDate = moment.tz('Asia/Pakistan').format('DD/MM/YYYY');
-                    const membersCount = metadata.participants.length;
-            sock.sendMessage(id, {
-               text: `> Goodbye @${userName} from ${metadata.subject}.\n> We are now ${membersCount} in the group.\n> Left at: ${leaveTime} on ${leaveDate}"`, contextInfo: {
-                  mentionedJid: [jid],
-                  externalAdReply: {
-                     title: `Leave`,
-                     mediaType: 1,
-                     previewType: 0,
-                     renderLargerThumbnail: true,
-                     thumbnailUrl: profile,
-                     sourceUrl: 'texhbybandali.blogspot.com'
-                  }
-               }
-            })
-         }
+    for (const jid of participants) {
+      let profile;
+      try {
+        profile = await sock.profilePictureUrl(jid, "image");
+      } catch {
+        profile = "https://i.ibb.co/S5B1k8Z/default.jpg"; // fallback image
       }
-   } catch (e) {
-      throw e
-   }
+
+      const userName = jid.split('@')[0];
+      const time = moment.tz('Asia/Karachi').format('HH:mm:ss');
+      const date = moment.tz('Asia/Karachi').format('DD/MM/YYYY');
+      const membersCount = metadata.participants.length;
+
+      if (action === "add" && config.WELCOME) {
+        await sock.sendMessage(id, {
+          text: `🎉 *Welcome @${userName}!* 🎉\n\n👤 Name: @${userName}\n📛 Group: *${metadata.subject}*\n🔢 Member No: *${membersCount}*\n⏰ Joined at: *${time}* on *${date}*\n\n_*Enjoy your stay!*_`,
+          contextInfo: {
+            mentionedJid: [jid],
+            externalAdReply: {
+              title: `✨ Welcome to ${metadata.subject}!`,
+              body: `@${userName} joined the party!`,
+              mediaType: 1,
+              previewType: 0,
+              renderLargerThumbnail: true,
+              thumbnailUrl: profile,
+              sourceUrl: 'https://techbybandali.blogspot.com'
+            }
+          }
+        });
+      }
+
+      else if (action === "remove" && config.WELCOME) {
+        await sock.sendMessage(id, {
+          text: `👋 *Goodbye @${userName}!* 👋\n\n📛 Group: *${metadata.subject}*\n👥 Members Left: *${membersCount}*\n⏰ Left at: *${time}* on *${date}*\n\n_*We’ll miss you!*_`,
+          contextInfo: {
+            mentionedJid: [jid],
+            externalAdReply: {
+              title: `👋 Left ${metadata.subject}`,
+              body: `@${userName} has left the group.`,
+              mediaType: 1,
+              previewType: 0,
+              renderLargerThumbnail: true,
+              thumbnailUrl: profile,
+              sourceUrl: 'https://techbybandali.blogspot.com'
+            }
+          }
+        });
+      }
+    }
+  } catch (e) {
+    console.error("Error in GroupParticipants handler:", e);
+  }
 }
