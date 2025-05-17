a2f390e7b4525536d2d0f73ad41902788762e4fa
Bandaheali <bandaheali.maree@gmail.com>
2025-05-17 11:41:46 +0500

    Update Control-mode.js

--- bandaheali/Maree/Control-mode.js
+++ bandaheali/Maree/Control-mode.js
@@ -18,9 +18,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
         if (['public', 'private'].includes(text)) {
             if (text === 'public') {
-                Matrix.public = true;
-               Sarkar.MODE = "public";
-               setSetting('mode'  "public");
+               setSetting('mode', "public");
                 m.reply(`*_MODE CHANGED SUCCESSFULLY NOW I AM IN ${text} MODE_*`);
             } else if (text === 'private') {
                 Matrix.public = false;
