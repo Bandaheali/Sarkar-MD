f780454b49daf319f15518a014120085d83d2eb1
Bandaheali <bandaheali.maree@gmail.com>
2025-05-19 11:02:37 +0500

    Update call-handler.js

--- bandaheali/Sarkar/call-handler.js
+++ bandaheali/Sarkar/call-handler.js
@@ -1,10 +1,10 @@
 import config from '../../config.js';
-
+const sarkar = config.CALL_MSG;
 const Callupdate = async (json, sock) => {
    for (const id of json) {
       if (id.status === 'offer' && config.REJECT_CALL) {
          let msg = await sock.sendMessage(id.from, {
-            text: `*𝗦𝗢𝗢𝗥𝗬 𝗙𝗢𝗥 𝗗𝗘𝗖𝗟𝗜𝗡𝗘 𝗬𝗢𝗨𝗥 𝗖𝗔𝗟𝗟*\n\n*KEEP USING SARKAR-MD*`,
+            text: `*${sarkar}*\n*KEEP USING SARKAR-MD*`,
             mentions: [id.from],
          });
          await sock.rejectCall(id.id, id.from);
