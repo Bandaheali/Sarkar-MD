eafedf2f58e127d88e5c279f506e76543b61b22e
Bandaheali <bandaheali.maree@gmail.com>
2025-06-18 06:15:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -22,21 +22,21 @@ const config = {
   CHAT_BOT: toBool(getSetting('chatbot') || process.env.CHAT_BOT),
   VOICE_BOT: toBool(getSetting('voicebot') || process.env.VOICE_BOT),
   ANTI_DELETE: toBool(getSetting('antidelete') || process.env.ANTI_DELETE),
-  DELETE_PATH: getSetting('deletepath') || process.env.DELETE_PATH || "same",
+  DELETE_PATH: getSetting('deletepath') || process.env.DELETE_PATH || "pm",
   AUTO_REACT: toBool(getSetting('autoreact') || process.env.AUTO_REACT),
   HEART_REACT: toBool(getSetting('heartreact') || process.env.HEART_REACT),
   AUTO_TYPING: toBool(getSetting('typing') || process.env.AUTO_TYPING),
   AUTO_RECORDING: toBool(getSetting('recording') || process.env.AUTO_RECORDING),
   ALWAYS_ONLINE: toBool(getSetting('alwaysonline') || process.env.ALWAYS_ONLINE),
-  AUTO_BIO: toBool(getSetting('autobio') || process.env.AUTO_BIO),
+  AUTO_BIO: toBool(getSetting('autobio') || process.env.AUTO_BIO, false),
   AUTO_STATUS_SEEN: toBool(getSetting('statusview') || process.env.AUTO_STATUS_SEEN, true),
-  AUTO_STATUS_REACT: toBool(getSetting('statusreact') || process.env.AUTO_STATUS_REACT, true),
+  AUTO_STATUS_REACT: toBool(getSetting('statusreact') || process.env.AUTO_STATUS_REACT, false),
   PM_BLOCK: toBool(getSetting('pmblock') || process.env.PM_BLOCK),
-  REJECT_CALL: toBool(getSetting('antiCall') || process.env.REJECT_CALL),
+  REJECT_CALL: toBool(getSetting('anticall') || process.env.REJECT_CALL),
   MODE: getSetting('mode') || process.env.MODE || 'public',
   MENU_IMAGE: getSetting('menuimage') || process.env.MENU_IMAGE || 'https://files.catbox.moe/yd6y5b.jpg',
   BOT_NAME: getSetting('botname') || process.env.BOT_NAME || "Sarkar-MD",
-  CALL_MSG: getSetting('callmsg') || process.env.CALL_MSG || "Hey there!*\n\nSorry to inform you, but *Anti-Call protection* is currently active on this number. That’s why your call was automatically disconnected.\n\nPlease don’t take it the wrong way — calling is disabled to avoid disturbance. If there’s something important, kindly *drop your message here* and we’ll get back to you as soon as possible.\n\nThanks for your understanding!\n"
+  CALL_MSG: getSetting('callmsg') || process.env.CALL_MSG || "_Soory For rejecting The Call i Am Busy Right Now drop Your Msg Here_"
 };
 
 export default config;
